fa068f68896d2a470d30f55d91fd6fa56bd9eefc
Francesco Zangrillo <zangrillo2@unibo.it>
2024-09-25 17:14:09 +0200

    eval code

--- ms_train.py
+++ ms_train.py
@@ -1,104 +1,104 @@
-from unsloth import FastLanguageModel 
-from unsloth import is_bfloat16_supported
+
+from unsloth import FastLanguageModel
 import torch
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
 import json
-import pandas as pd
+from datasets import Dataset
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 from transformers import TrainingArguments
-from datasets import Dataset
+from unsloth import is_bfloat16_supported
 import os
 os.environ["WANDB_DISABLED"] = "true"
 
-max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
-dtype = None
-load_in_4bit = True
 
-
-corrected_dataset = []
-with open("chunk1.json", 'r') as f:
-    corrected_dataset = json.load(f)
-
-# Create a prompt string by iterating through the corrected dataset
-# Assume each data point in corrected_dataset has keys 'question' and 'answer'
-formatted_dataset = []
-for data in corrected_dataset:
-    prompt_text = f"""###Contexts: 
-{data['selected_passages']}
-
-### Question: 
-{data['query']}
-
-### Answer:{data['answers']}
-    """
-    data['text'] = prompt_text  # Add the prompt to a new 'text' field
-    formatted_dataset.append(data)
-
-output_json_path = "formatted_dataset.json"
-with open(output_json_path, 'w', encoding='utf-8') as f:
-    json.dump(formatted_dataset, f, indent=4, ensure_ascii=False)
-
-print(f"Formatted dataset saved to {output_json_path}")
-#print(formatted_dataset[0]['text'])
-
-
-
-# Convert the list of dictionaries to a Huggingface Dataset
-dataset = Dataset.from_pandas(pd.DataFrame(formatted_dataset))
-
-
-print(dataset[0])
-
-
-# Load model and tokenizer
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name = "unsloth/Meta-Llama-3.1-8B",
     max_seq_length = max_seq_length,
     dtype = dtype,
     load_in_4bit = load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
 )
-
-# Do model patching and add fast LoRA weights
 model = FastLanguageModel.get_peft_model(
     model,
-    r = 16,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
     target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
-                      "gate_proj", "up_proj", "down_proj"],
+                      "gate_proj", "up_proj", "down_proj",],
     lora_alpha = 16,
     lora_dropout = 0, # Supports any, but = 0 is optimized
     bias = "none",    # Supports any, but = "none" is optimized
     # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
     use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
     random_state = 3407,
-    max_seq_length = max_seq_length,
     use_rslora = False,  # We support rank stabilized LoRA
     loftq_config = None, # And LoftQ
 )
 
-response_template = "### Answer:"
-collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+
+prompt = """Provide a clear and concise answer to the question using only the information from the passage.
+### Passage:
+{}
+### Question:
+{}
+
+### Answer: {}"""
+
+EOS_TOKEN = tokenizer.eos_token # Must add EOS_TOKEN
+def formatting_prompts_func(examples):
+    query = examples["query"]
+    context       = examples["selected_passages"]
+    outputs      = examples["answers"]
+    texts = []
+    for q, c, a in zip(query, context, outputs):
+        # Must add EOS_TOKEN, otherwise your generation will go on forever!
+        text = prompt.format(q, c, a) + EOS_TOKEN
+        texts.append(text)
+    return { "text" : texts, }
+pass
+
+corrected_dataset = []
+with open("chunk1.json", 'r') as f:
+    corrected_dataset = json.load(f)
+
+corrected_dataset = Dataset.from_list(corrected_dataset)
+
+dataset = corrected_dataset.map(formatting_prompts_func, batched = True,)
+
+# response_template = "### Answer: "
+# collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
+
 
 trainer = SFTTrainer(
     model = model,
+    tokenizer = tokenizer,
     train_dataset = dataset,
     dataset_text_field = "text",
-    data_collator = collator,
+    #data_collator = collator,
     max_seq_length = max_seq_length,
-    tokenizer = tokenizer,
+    dataset_num_proc = 2,
+    packing = False, # Can make training 5x faster for short sequences.
     args = TrainingArguments(
         per_device_train_batch_size = 2,
         gradient_accumulation_steps = 4,
-        warmup_steps = 10,
-        max_steps = 5, #  it will override any value given in num_train_epochs
+        warmup_steps = 5,
+        # num_train_epochs = 1, # Set this for 1 full training run.
+        max_steps = 3,
+        learning_rate = 2e-4,
         fp16 = not is_bfloat16_supported(),
         bf16 = is_bfloat16_supported(),
         logging_steps = 1,
-        output_dir = "outputs",
         optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
         seed = 3407,
+        output_dir = "outputs",
     ),
 )
 
 
+
+#@title Show current memory stats
 gpu_stats = torch.cuda.get_device_properties(0)
 start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
 max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
@@ -109,6 +109,10 @@ print(f"{start_gpu_memory} GB of memory reserved.")
 trainer_stats = trainer.train()
 
 
+model.save_pretrained("lora_model") # Local saving
+tokenizer.save_pretrained("lora_model")
+
+#@title Show final memory and time stats
 used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
 used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
 used_percentage = round(used_memory         /max_memory*100, 3)
@@ -119,10 +123,3 @@ print(f"Peak reserved memory = {used_memory} GB.")
 print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
 print(f"Peak reserved memory % of max memory = {used_percentage} %.")
 print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
-
-# Go to https://github.com/unslothai/unsloth/wiki for advanced tips like
-# (1) Saving to GGUF / merging to 16bit for vLLM
-# (2) Continued training from a saved LoRA adapter
-# (3) Adding an evaluation loop / OOMs
-# (4) Customized chat templates
-
